099e76f74cc77a3b04748cef4f283ba9b2ffc2c2
singlearray <zoltan.bathori@telusinternational.com>
2025-02-17 13:29:09 +0200

    first commit

---
+++ src/TestComponent/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './index.css';
+
+const TestComponent = ({ }) => {
+
+    return <div id="testComponent">
+        Start coding here...
+    </div>
+}
+
+export default TestComponent;
