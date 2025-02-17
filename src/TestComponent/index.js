8b7c6f93e7b91e9ee1fd9922bf378b0cc752b3f2
singlearray <zoltan.bathori@telusinternational.com>
2025-02-17 13:42:02 +0200

    first commit

--- src/TestComponent/index.js
+++ src/TestComponent/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import './index.css';
+import Solution from './Solution';
 
 const TestComponent = ({ }) => {
+    // Datasource:
+    // https://dummyjson.com/users
 
     return <div id="testComponent">
+        <a href="https://example-web-app-b6e1e.web.app" target="_blank">Solution</a><br /><br />
         Start coding here...
+        <Solution />
     </div>
 }
 
