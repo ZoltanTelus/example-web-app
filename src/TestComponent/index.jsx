872a376dffeffff78a2cd3e095e068103544945d
singlearray <zoltan.bathori@telusinternational.com>
2025-07-15 15:10:52 +0300

    switching to vite

--- src/TestComponent/index.jsx
+++ src/TestComponent/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import './index.css';
+// import Solution from './Solution';
 
 const TestComponent = ({ }) => {
     // Datasource:
     // https://dummyjson.com/users
 
     return <div id="testComponent">
-        <a href="https://example-web-app-b6e1e.web.app" target="_blank">Solution</a><br /><br />
+        {/* <Solution /> */}
+        <a href="https://example-web-app-8d97a.web.app" target="_blank">Solution</a><br /><br />
         Start coding here! <br />
         Don't forget about the mobile view...
     </div>
